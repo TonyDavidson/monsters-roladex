9c995d2638dd0f5842cec61ad1322617907afd06
TonyDavidson <tonydavidson@iinet.net.au>
2019-08-09 15:33:12 +0800

    Updates

---
+++ precache-manifest.41e21819b7a088321e2d3a7624c7662b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9777b2c0eb71c69e5e07564510d2c4d",
+    "url": "/monsters-roladex/index.html"
+  },
+  {
+    "revision": "f12c87bb0ead6bf1c7a4",
+    "url": "/monsters-roladex/static/css/main.1777e531.chunk.css"
+  },
+  {
+    "revision": "f9544c9e233f918591b1",
+    "url": "/monsters-roladex/static/js/2.e6ff8d7d.chunk.js"
+  },
+  {
+    "revision": "f12c87bb0ead6bf1c7a4",
+    "url": "/monsters-roladex/static/js/main.bda3b0ab.chunk.js"
+  },
+  {
+    "revision": "5a493b1bbf27f5b4a899",
+    "url": "/monsters-roladex/static/js/runtime~main.8026f8c8.js"
+  }
+]);
